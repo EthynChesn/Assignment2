8715e806db9cff0b56dda3d63ceedaf740296ad5
EthynChesn <echesnut26@cornellcollege.edu>
2025-04-24 21:23:41 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -2,12 +2,35 @@ from socket import *
 from threading import *
 import sys
 
+PrivateMode = False
+
 def Send():
-    Outgoing = input()
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
-    clientSocket.send(Outgoing.encode())
-    print(IPAddr + ': ' + Outgoing)
+    global PrivateMode
+    while True:
+        if PrivateMode == True:
+            Outgoing = input('Enter Private Message: ')
+        else:
+            Outgoing = input()
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+        if Outgoing == '/private':
+            PrivateMode = True
+            recipient = input('Enter user ID of intended recipient: ')
+            clientSocket.send(recipient.encode())
+        elif Outgoing == '/end':
+            if PrivateMode == True:
+                PrivateMode = False
+                print('Ended Private Chat')
+                clientSocket.send(Outgoing.encode())
+            else:
+                print('You are not in a Private Chat.')
+                clientSocket.send(Outgoing.encode())
+        else:
+            clientSocket.send(Outgoing.encode())
+            if PrivateMode == True:
+                print('[Private Message]'  + IPAddr + ': ' + Outgoing)
+            else:
+                print(IPAddr + ': ' + Outgoing)
 
 hostname = gethostname()
 IPAddr = gethostbyname(hostname)
@@ -25,4 +48,5 @@ while True:
     SendThread = Thread(target = Send)
     SendThread.start()
     Incoming = clientSocket.recv(1024).decode()
+    sys.stdout.write('\x1b[2K')
     print(Incoming)
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from socket import *
 from threading import *
 import sys
 
+serverSocket =  socket(AF_INET, SOCK_STREAM)
 clientlist = []
 clientdict = {}
 userlist = []
@@ -11,60 +12,81 @@ def NewClient(clientSocket, addr):
     global clientdict
     clientlist += [clientSocket]
     clientdict[str(addr[1])] = clientSocket
+    recipient = 'Server'
     while True:
         Incoming = clientSocket.recv(1024).decode()
-        if Incoming == 'close':
+        if Incoming == '/close':
             break
-        print(str(addr[0]) + ': ' + Incoming)
-        Incoming = str(addr[0]) + ': ' + Incoming
-        for c in clientlist:
-            if not c == clientSocket:
-                c.send(Incoming.encode())
+        if recipient == 'Server':
+            if Incoming in userlist:
+                recipient = Incoming
+            else:
+                print(str(addr[1]) + ': ' + Incoming)
+                Incoming = str(addr[1]) + ': ' + Incoming
+                for c in clientlist:
+                    if not c == clientSocket:
+                        c.send(Incoming.encode())
+        else:
+            if Incoming == '/end':
+                print('recieved /end')
+                recipient == 'Server'
+                print(recipient)
+            else:
+                Incoming = '[Private Message]  ' + str(addr[1]) + ': ' + Incoming
+                clientdict[recipient].send(Incoming.encode())
+        
     clientSocket.close()
     clientlist.remove(clientSocket)
+    del clientdict[str(addr[1])]
+    userlist.remove(str(addr[1]))
 
 def Broadcast():
-    global userlist
-    Outgoing = input()
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
-    Outgoing = 'Server Broadcast: ' + Outgoing
-    print(Outgoing)
-    for c in clientlist:
-        c.send(Outgoing.encode())
+    while True:
+        global userlist
+        Outgoing = input()
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+        if Outgoing == '/shutdown':
+            print('Server Shut Down')
+            for c in clientlist:
+                c.close()
+            serverSocket.close()
+        else:
+            Outgoing = 'Server Broadcast: ' + Outgoing
+            print(Outgoing)
+            for c in clientlist:
+                c.send(Outgoing.encode())
 
 def main():
+    global serverSocket
     global userlist
     global clientlist
     Host = '0.0.0.0'
     Port = 12345
-
-    serverSocket =  socket(AF_INET, SOCK_STREAM)
 
     serverSocket.bind((Host, Port))
 
     serverSocket.listen(5)
 
     print('Server Open. Waiting for Clients to Connect...')
-    serverMessage = 'Server Broadcast: Thank you for connecting!' + '\n'
+    serverMessage = 'Server Broadcast: Welcome, thank you for connecting!' + '\n'
 
     while True:
         BroadcastThread = Thread(target=Broadcast)
         BroadcastThread.start()
         Connection, addr = serverSocket.accept()
-        userlist += [addr[0]]
+        userlist += [str(addr[1])]
         ClientThread = Thread(target=NewClient, args=(Connection,addr))
         ClientThread.start()
         print('Got Connection', addr)
         for c in clientlist:
-            msg = 'Server Broadcast: ' + addr[0] + ' has joined!' + '\n'
+            msg = 'Server Broadcast: ' + str(addr[1]) + ' has joined!' + '\n'
             c.send(msg.encode())
         Connection.send(serverMessage.encode())
-        Connection.send('Current members: '.encode())
+        Connection.send('Current users:\n'.encode())
         for user in userlist:
             user = user + '\n'
             Connection.send(user.encode())
-
 
 if __name__ == '__main__':
     main()
